493d2827aaf02b7dc0d2bfe37e6adba0430b20df
Gabriel Carvalho <gabriel.carvalho@soulasalle.com.br>
2024-03-11 16:42:57 -0300

    primeiro commit

---
+++ FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,18 @@
+﻿using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers;
+
+[ApiController]
+[Route("[Controller]")]
+public class FilmeController : ControllerBase
+{
+    private static List<Filme> filmes = new List<Filme>();
+
+    public void AdcionarFilme([FromBody] Filme filme)
+    {
+        filmes.Add(filme);
+        Console.WriteLine(filme.Titulo);
+        Console.WriteLine(filme.Duracao);
+    }
+}
